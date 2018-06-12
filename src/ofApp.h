5d9906342badd4d4e498e7e61a160a16d1cc6e88
M1mez <Fpscorner@gmail.com>
2018-06-12 14:33:47 +0200

    try automating shooting star on mouseclick

--- src/ofApp.h
+++ src/ofApp.h
@@ -21,4 +21,8 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+		//objects
+		ofSpherePrimitive SkyDome;
+		ofTexture SkyDomeTexture;
+
 };
